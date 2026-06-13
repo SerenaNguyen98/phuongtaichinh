ae3762227a94fbf16ab0ff09f19e5020a87fc809
serena <ntthuynga1998@gmail.com>
2026-06-13 20:46:21 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  outputFileTracingRoot: "d:/0. vibe code/phuongtaichinh/phuongtaichinh",
   images: {
     remotePatterns: [
       {
